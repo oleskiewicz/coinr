e650647644166e75b3e37a2736cba1170bb78be8
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2019-02-22 21:44:57 +0000

    add server & ui

---
+++ app/ui.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(shinydashboard)
+
+
+shinyUI(dashboardPage(
+  dashboardHeader(title = "coinr"),
+
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("th-large")),
+      menuItem("Historical rates", tabName = "history", icon = icon("calendar")),
+      menuItem("Histogram", tabName = "histogram"),
+      menuItem("Portfolio", tabName = "portfolio", icon = icon("money")),
+      menuItem("Source", tabName = "github", icon = icon("code"))
+    )
+  ),
+
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "home",
+        h2("Home")
+      ),
+
+      tabItem(
+        tabName = "history",
+        h2("Historical rates")
+      ),
+
+      tabItem(
+        tabName = "histogram",
+        h2("Histogram"),
+
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("bins",
+              "Number of bins:",
+              min = 1,
+              max = 50,
+              value = 30
+            )
+          ),
+
+          mainPanel(
+            plotOutput("distPlot")
+          )
+        )
+      ),
+
+      tabItem(
+        tabName = "portfolio",
+        h2("Portfolio")
+      )
+    )
+  )
+))
