2f587a8779a63d16721fad7d22100cebb54318da
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2019-02-24 18:00:03 +0000

    add historical prices page

--- app/server.R
+++ app/server.R
@@ -1,11 +1,36 @@
+library(coindeskr)
+library(dygraphs)
 library(shiny)
 library(shinydashboard)
 
-shinyServer(function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    hist(x, breaks = bins, col = "darkgray", border = "white")
+shinyServer(function(input, output) {
+  last31 <- get_last31days_price()
+
+  output$minprice <- renderValueBox({
+    valueBox(
+      value = min(last31),
+      subtitle = "31 days minimum",
+      icon = icon("arrow-down")
+    )
   })
+
+  output$maxprice <- renderValueBox({
+    valueBox(
+      value = max(last31),
+      subtitle = "31 days maximum",
+      icon = icon("arrow-up")
+    )
+  })
+
+  output$btcprice <- renderDygraph(
+    dygraph(data = last31, main = "Bitcoin USD Price for Last 31 days") %>%
+      dyHighlight(
+        highlightCircleSize = 5,
+        highlightSeriesBackgroundAlpha = 0.2,
+        hideOnMouseOut = FALSE,
+        highlightSeriesOpts = list(strokeWidth = 3)
+      ) %>%
+      dyRangeSelector()
+  )
 })
--- app/ui.R
+++ app/ui.R
@@ -7,9 +7,7 @@ shinyUI(dashboardPage(
 
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Home", tabName = "home", icon = icon("th-large")),
       menuItem("Historical rates", tabName = "history", icon = icon("calendar")),
-      menuItem("Histogram", tabName = "histogram"),
       menuItem("Portfolio", tabName = "portfolio", icon = icon("money")),
       menuItem("Source", href = "//github.com/oleskiewicz/coinr", icon = icon("code"))
     )
@@ -17,42 +15,21 @@ shinyUI(dashboardPage(
 
   dashboardBody(
     tabItems(
-
-      tabItem(
-        tabName = "home",
-        h2("Home")
-      ),
-
       tabItem(
         tabName = "history",
-        h2("Historical rates")
-      ),
 
-      tabItem(
-        tabName = "histogram",
-        h2("Histogram"),
+        fluidRow(
+          valueBoxOutput("minprice"),
+          valueBoxOutput("maxprice")
+        ),
 
-        sidebarLayout(
-          sidebarPanel(
-            sliderInput("bins",
-              "Number of bins:",
-              min = 1,
-              max = 50,
-              value = 30
-            )
-          ),
-
-          mainPanel(
-            plotOutput("distPlot")
-          )
-        )
+        dygraphOutput("btcprice")
       ),
 
       tabItem(
         tabName = "portfolio",
         h2("Portfolio")
       )
-
     )
   )
 ))
